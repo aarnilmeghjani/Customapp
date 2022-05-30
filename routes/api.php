e20af333fb8e26dcee8e8a21acc5ce7b06c520c0
aarnilmeghjani <aarnil@hulkapps.com>
2022-05-30 12:18:49 +0530

    testing

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::any('products',[ProductController::class,'index']);
 Route::delete('product/delete/{id}',[ProductController::class,'destroy']);
 Route::post('product/update',[ProductController::class,'update']);
